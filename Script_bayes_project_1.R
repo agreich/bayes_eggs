c6e4c983bc5c36c7c885a006c858429dc84492d0
Alexandra Reich <agreich@alaska.edu>
2022-11-03 15:11:44 -0800

    Initial commit

---
+++ Script_bayes_project_1.R
@@ -0,0 +1,10 @@
+#Alexandra Reich
+#Bayes project: egg diameter
+#last updated: 11/03/22
+
+
+#####Proposal stuff######
+#first, let's load some data. I'll use the cleaned dataset from my other analyses
+
+
+#Let's get some exploratory graphs for the project proposal
